ecf6c0fc009ce28bb2ee0b1495ea035178163512
Tomi Valkeinen <tomba@iki.fi>
2008-02-12 21:39:12 +0200

    growinglocationgrid

---
+++ Common/GrowingLocationGrid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGame
+{
+	public class GrowingLocationGrid<T>
+	{
+		LocationGrid<LocationGrid<T>> m_grid;
+
+		public GrowingLocationGrid()
+		{
+			m_grid = new LocationGrid<LocationGrid<T>>(3, 3);
+		}
+	}
+}
