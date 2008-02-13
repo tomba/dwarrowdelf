d8ed94023d7f5ca8c1d93e0d4161491ea96c8f25
Tomi Valkeinen <tomba@iki.fi>
2008-02-13 21:56:13 +0200

    more properties for intrect

--- Common/IntRect.cs
+++ Common/IntRect.cs
@@ -21,12 +21,37 @@ namespace MyGame
 			this.Height = height;
 		}
 
+		public int Left
+		{
+			get { return X; }
+		}
+
+		public int Right
+		{
+			get { return X + Width; }
+		}
+
+		public int Top
+		{
+			get { return Y; }
+		}
+
+		public int Bottom
+		{
+			get { return Y + Height; }
+		}
+
 		public bool Contains(Location l)
 		{
 			if (l.X < this.X || l.Y < this.Y || l.X >= this.X + this.Width || l.Y >= this.Y + this.Height)
 				return false;
 			else
 				return true;
+		}
+
+		public override string ToString()
+		{
+			return ("{X=" + this.X + ",Y=" + this.Y + ",Width=" + this.Width + ",Height=" + this.Height + "}");
 		}
 	}
 }
