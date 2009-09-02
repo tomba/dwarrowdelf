21f220b754c8252bd8a681919fa1e2e11670b388
Tomi Valkeinen <tomba@iki.fi>
2009-09-02 22:26:49 +0300

    foo

--- Common/DataTypes/LocationGrid.cs
+++ Common/DataTypes/LocationGrid.cs
@@ -6,25 +6,6 @@ using System.Collections;
 
 namespace MyGame
 {
-	public class Grid2DBase<T>
-	{
-		public int Width { get; private set; }
-		public int Height { get; private set; }
-
-		public Grid2DBase(int width, int height)
-		{
-			this.Width = width;
-			this.Height = height;
-			this.Grid = new T[width * height];
-		}
-
-		protected T[] Grid { get; private set; }
-
-		protected int GetIndex(IntPoint p)
-		{
-			return p.Y * this.Width + p.X;
-		}
-	}
 
 	/**
 	 * 2D grid made of <T>s
