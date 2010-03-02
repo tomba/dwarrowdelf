b2511fa4cef2b7491473bca76d1da0c75a2cb48d
Tomi Valkeinen <tomba@iki.fi>
2010-03-02 22:57:51 +0200

    add properties file

--- Common/Changes.cs
+++ Common/Changes.cs
@@ -100,44 +100,6 @@ namespace MyGame
 		public ClientMsgs.Message ObjectData { get; set; }
 	}
 
-	public enum PropertyID : ushort
-	{
-		None,
-
-		VisionRange,
-		Color,
-
-		HitPoints,
-		SpellPoints,
-
-		Strength,
-		Dexterity,
-		Constitution,
-		Intelligence,
-		Wisdom,
-		Charisma,
-	}
-
-	public class PropertyDefinition
-	{
-		public PropertyDefinition(PropertyID id, PropertyVisibility visibility, object defaultValue)
-		{
-			this.PropertyID = id;
-			this.Visibility = visibility;
-			this.DefaultValue = defaultValue;
-		}
-
-		public PropertyID PropertyID { get; private set; }
-		public PropertyVisibility Visibility { get; private set; }
-		public object DefaultValue { get; private set; }
-	}
-
-	public enum PropertyVisibility
-	{
-		Public,		// everybody see
-		Friendly,	// friendlies see
-	}
-
 	public class PropertyChange : ObjectChange
 	{
 		public PropertyChange(IIdentifiable ob, PropertyDefinition property, object value)
---
+++ Common/Properties.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGame
+{
+	public enum PropertyID : ushort
+	{
+		None,
+
+		VisionRange,
+		Color,
+
+		HitPoints,
+		SpellPoints,
+
+		Strength,
+		Dexterity,
+		Constitution,
+		Intelligence,
+		Wisdom,
+		Charisma,
+	}
+
+	public class PropertyDefinition
+	{
+		public PropertyDefinition(PropertyID id, PropertyVisibility visibility, object defaultValue)
+		{
+			this.PropertyID = id;
+			this.Visibility = visibility;
+			this.DefaultValue = defaultValue;
+		}
+
+		public PropertyID PropertyID { get; private set; }
+		public PropertyVisibility Visibility { get; private set; }
+		public object DefaultValue { get; private set; }
+	}
+
+	public enum PropertyVisibility
+	{
+		Public,		// everybody see
+		Friendly,	// friendlies see
+	}
+}
