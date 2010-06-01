c36387d5cc2e3c26db9194907883de9e5910b8d9
Tomi Valkeinen <tomba@iki.fi>
2010-06-01 21:47:45 +0300

    add missing file

---
+++ WPFClientControls/IBitmapGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace MyGame.Client
+{
+	public interface IBitmapGenerator
+	{
+		BitmapSource GetBitmap(SymbolID symbolID, Color color, bool dark);
+		int NumDistinctBitmaps { get; }
+		int TileSize { get; set; }
+	}
+}
