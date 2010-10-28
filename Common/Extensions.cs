d6c65a21ccda73294e7b60b3e7ff64e04467f067
Tomi Valkeinen <tomba@iki.fi>
2010-10-28 20:33:18 +0300

    add capitalize()

---
+++ Common/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dwarrowdelf
+{
+	public static class Extensions
+	{
+		public static string Capitalize(this string str)
+		{
+			if (String.IsNullOrEmpty(str))
+				return String.Empty;
+
+			char[] arr = str.ToCharArray();
+			arr[0] = Char.ToUpperInvariant(arr[0]);
+			return new String(arr);
+		}
+	}
+}
