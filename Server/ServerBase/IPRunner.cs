7d9b2b39221fec797ae05c4a2ff602a470ef212d
Tomi Valkeinen <tomba@iki.fi>
2010-11-17 21:03:12 +0200

    move IP stuff to a separate class

---
+++ Server/ServerBase/IPRunner.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Scripting.Hosting;
+
+namespace Dwarrowdelf.Server
+{
+	class IPRunner
+	{
+		ScriptEngine m_scriptEngine;
+		ScriptScope m_scriptScope;
+		MyStream m_scriptOutputStream;
+
+		Action<Messages.ServerMessage> m_sender;
+
+		public IPRunner(World world, Action<Messages.ServerMessage> sender)
+		{
+			m_sender = sender;
+			m_scriptOutputStream = new MyStream(sender);
+
+			m_scriptEngine = IronPython.Hosting.Python.CreateEngine();
+			m_scriptEngine.Runtime.IO.SetOutput(m_scriptOutputStream, System.Text.Encoding.Unicode);
+			m_scriptEngine.Runtime.IO.SetErrorOutput(m_scriptOutputStream, System.Text.Encoding.Unicode);
+
+			m_scriptScope = m_scriptEngine.CreateScope();
+			m_scriptScope.SetVariable("world", world);
+			m_scriptScope.SetVariable("get", new Func<object, IBaseGameObject>(world.IPGet));
+
+			m_scriptEngine.Execute("import clr", m_scriptScope);
+			m_scriptEngine.Execute("clr.AddReference('Dwarrowdelf.Common')", m_scriptScope);
+			m_scriptEngine.Execute("import Dwarrowdelf", m_scriptScope);
+		}
+
+		public void Exec(string script)
+		{
+			try
+			{
+				var r = m_scriptEngine.ExecuteAndWrap(script, m_scriptScope);
+				m_scriptScope.SetVariable("ret", r);
+				m_scriptEngine.Execute("print ret", m_scriptScope);
+			}
+			catch (Exception e)
+			{
+				var str = "IP error:\n" + e.Message + "\n";
+				m_sender(new Messages.IPOutputMessage() { Text = str });
+			}
+		}
+
+		class MyStream : Stream
+		{
+			Action<Messages.ServerMessage> m_sender;
+			MemoryStream m_stream = new MemoryStream();
+
+			public MyStream(Action<Messages.ServerMessage> sender)
+			{
+				m_sender = sender;
+			}
+
+			public override bool CanRead { get { return false; } }
+			public override bool CanSeek { get { return false; } }
+			public override bool CanWrite { get { return true; } }
+
+			public override void Flush()
+			{
+				if (m_stream.Position == 0)
+					return;
+
+				var text = System.Text.Encoding.Unicode.GetString(m_stream.GetBuffer(), 0, (int)m_stream.Position);
+				m_stream.Position = 0;
+				m_stream.SetLength(0);
+				var msg = new Messages.IPOutputMessage() { Text = text };
+				m_sender(msg);
+			}
+
+			public override long Length { get { throw new NotImplementedException(); } }
+
+			public override long Position
+			{
+				get { throw new NotImplementedException(); }
+				set { throw new NotImplementedException(); }
+			}
+
+			public override int Read(byte[] buffer, int offset, int count)
+			{
+				throw new NotImplementedException();
+			}
+
+			public override long Seek(long offset, SeekOrigin origin)
+			{
+				throw new NotImplementedException();
+			}
+
+			public override void SetLength(long value)
+			{
+				throw new NotImplementedException();
+			}
+
+			public override void Write(byte[] buffer, int offset, int count)
+			{
+				if (count == 0)
+					return;
+
+				m_stream.Write(buffer, offset, count);
+			}
+		}
+
+
+
+	}
+}
--- Server/ServerBase/ServerConnection.cs
+++ Server/ServerBase/ServerConnection.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using Dwarrowdelf.Messages;
-using System.IO;
-using System.Diagnostics;
-using System.Collections.ObjectModel;
 
 namespace Dwarrowdelf.Server
 {
@@ -60,69 +60,7 @@ namespace Dwarrowdelf.Server
 		IConnection m_connection;
 		bool m_userLoggedIn;
 
-		Microsoft.Scripting.Hosting.ScriptEngine m_scriptEngine;
-		Microsoft.Scripting.Hosting.ScriptScope m_scriptScope;
-
-		MyStream m_scriptOutputStream;
-
-		class MyStream : Stream
-		{
-			Action<Messages.ServerMessage> m_sender;
-			MemoryStream m_stream = new MemoryStream();
-
-			public MyStream(Action<Messages.ServerMessage> sender)
-			{
-				m_sender = sender;
-			}
-
-			public override bool CanRead { get { return false; } }
-			public override bool CanSeek { get { return false; } }
-			public override bool CanWrite { get { return true; } }
-
-			public override void Flush()
-			{
-				if (m_stream.Position == 0)
-					return;
-
-				var text = System.Text.Encoding.Unicode.GetString(m_stream.GetBuffer(), 0, (int)m_stream.Position);
-				m_stream.Position = 0;
-				m_stream.SetLength(0);
-				var msg = new Messages.IPOutputMessage() { Text = text };
-				m_sender(msg);
-			}
-
-			public override long Length { get { throw new NotImplementedException(); } }
-
-			public override long Position
-			{
-				get { throw new NotImplementedException(); }
-				set { throw new NotImplementedException(); }
-			}
-
-			public override int Read(byte[] buffer, int offset, int count)
-			{
-				throw new NotImplementedException();
-			}
-
-			public override long Seek(long offset, SeekOrigin origin)
-			{
-				throw new NotImplementedException();
-			}
-
-			public override void SetLength(long value)
-			{
-				throw new NotImplementedException();
-			}
-
-			public override void Write(byte[] buffer, int offset, int count)
-			{
-				if (count == 0)
-					return;
-
-				m_stream.Write(buffer, offset, count);
-			}
-		}
-
+		IPRunner m_ipRunner;
 
 		public ServerConnection(IConnection conn, World world)
 		{
@@ -135,19 +73,7 @@ namespace Dwarrowdelf.Server
 			m_connection.ReceiveEvent += OnReceiveMessage;
 			m_connection.DisconnectEvent += OnDisconnect;
 
-			m_scriptOutputStream = new MyStream(Send);
-
-			m_scriptEngine = IronPython.Hosting.Python.CreateEngine();
-			m_scriptEngine.Runtime.IO.SetOutput(m_scriptOutputStream, System.Text.Encoding.Unicode);
-			m_scriptEngine.Runtime.IO.SetErrorOutput(m_scriptOutputStream, System.Text.Encoding.Unicode);
-
-			m_scriptScope = m_scriptEngine.CreateScope();
-			m_scriptScope.SetVariable("world", m_world);
-			m_scriptScope.SetVariable("get", new Func<object, IBaseGameObject>(m_world.IPGet));
-
-			m_scriptEngine.Execute("import clr", m_scriptScope);
-			m_scriptEngine.Execute("clr.AddReference('Dwarrowdelf.Common')", m_scriptScope);
-			m_scriptEngine.Execute("import Dwarrowdelf", m_scriptScope);
+			m_ipRunner = new IPRunner(world, Send);
 		}
 
 		protected void OnDisconnect()
@@ -262,8 +188,6 @@ namespace Dwarrowdelf.Server
 
 			if (m_charLoggedIn)
 				ReceiveMessage(new LogOffCharRequestMessage()); // XXX
-
-			m_scriptScope.RemoveVariable("world");
 
 			m_world.RemoveUser(this);
 			m_world.WorkEnded -= HandleEndOfWork;
@@ -402,7 +326,6 @@ namespace Dwarrowdelf.Server
 			if (!player.MoveTo(env, new IntPoint3D(10, 10, 9)))
 				throw new Exception("Unable to move player");
 
-			m_scriptScope.SetVariable("me", player);
 #if qwe
 			var pet = new Living(m_world);
 			pet.SymbolID = SymbolID.Monster;
@@ -454,8 +377,6 @@ namespace Dwarrowdelf.Server
 		void ReceiveMessage(LogOffCharRequestMessage msg)
 		{
 			Debug.Print("LogOffChar");
-
-			m_scriptScope.RemoveVariable("me");
 
 			foreach (var l in m_controllables)
 			{
@@ -529,19 +450,7 @@ namespace Dwarrowdelf.Server
 		{
 			Debug.Print("IronPythonCommand");
 
-			var script = msg.Text;
-
-			try
-			{
-				var r = m_scriptEngine.ExecuteAndWrap(script, m_scriptScope);
-				m_scriptScope.SetVariable("ret", r);
-				m_scriptEngine.Execute("print ret", m_scriptScope);
-			}
-			catch (Exception e)
-			{
-				var str = "IP error:\n" + e.Message + "\n";
-				Send(new IPOutputMessage() { Text = str });
-			}
+			m_ipRunner.Exec(msg.Text);
 		}
 
 
