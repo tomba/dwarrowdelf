6c35077382e93fd4a7824f65f241620a4a59293c
Tomi Valkeinen <tomba@iki.fi>
2011-08-09 09:24:05 +0300

    separate selection dock

---
+++ Client/Client/UI/DockSelectedControl.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dwarrowdelf.Client
+{
+	/// <summary>
+	/// Interaction logic for DockSelectedControl.xaml
+	/// </summary>
+	public partial class DockSelectedControl : UserControl
+	{
+		public DockSelectedControl()
+		{
+			this.InitializeComponent();
+		}
+	}
+
+	public class ListConverter<T> : IValueConverter
+	{
+		Func<T, string> m_converter;
+
+		public ListConverter(Func<T, string> itemConverter)
+		{
+			m_converter = itemConverter;
+		}
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value == null)
+				return "";
+
+			var list = (IEnumerable<T>)value;
+
+			return String.Join(", ", list.Select(item => m_converter(item)));
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	class MyInteriorsConverter : ListConverter<Tuple<InteriorInfo, MaterialInfo>>
+	{
+		public MyInteriorsConverter() : base(item => item.Item1.Name + " (" + item.Item2.Name + ")") { }
+	}
+
+	class MyTerrainsConverter : ListConverter<Tuple<TerrainInfo, MaterialInfo>>
+	{
+		public MyTerrainsConverter() : base(item => item.Item1.Name + " (" + item.Item2.Name + ")") { }
+	}
+
+	class MyWatersConverter : ListConverter<byte>
+	{
+		public MyWatersConverter() : base(item => item.ToString()) { }
+	}
+
+	class MyBuildingsConverter : ListConverter<BuildingObject>
+	{
+		public MyBuildingsConverter() : base(item => item.BuildingInfo.Name) { }
+	}
+
+	class MyGrassesConverter : ListConverter<bool>
+	{
+		public MyGrassesConverter() : base(item => item.ToString()) { }
+	}
+}
