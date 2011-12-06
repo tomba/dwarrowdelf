357b12188e965740a87dc0686f5fb824dbd62907
Tomi Valkeinen <tomba@iki.fi>
2011-12-06 12:15:02 +0200

    large ob cleanup

---
+++ Server/World/LargeGameObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dwarrowdelf.Server
+{
+	public abstract class LargeGameObject : BaseGameObject, ILargeGameObject
+	{
+		[SaveGameProperty]
+		public Environment Environment { get; private set; }
+		IEnvironment ILargeGameObject.Environment { get { return this.Environment as IEnvironment; } }
+
+		[SaveGameProperty]
+		public IntRectZ Area { get; private set; }
+
+		protected LargeGameObject(ObjectType objectType, IntRectZ area)
+			: base(objectType)
+		{
+			this.Area = area;
+		}
+
+		protected LargeGameObject(SaveGameContext ctx, ObjectType objectType)
+			: base(ctx, objectType)
+		{
+		}
+
+		public bool Contains(IntPoint3D point)
+		{
+			return this.Area.Contains(point);
+		}
+
+		protected void SetEnvironment(Environment env)
+		{
+			if (this.Environment != null)
+				this.Environment.RemoveLargeObject(this);
+
+			this.Environment = env;
+
+			if (this.Environment != null)
+				this.Environment.AddLargeObject(this);
+		}
+	}
+}
