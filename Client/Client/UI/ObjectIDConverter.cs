5b58a1f11f4e7d6e8f6922255021b11462290a25
Tomi Valkeinen <tomba@iki.fi>
2011-12-31 19:08:00 +0200

    treeview for objects

---
+++ Client/Client/UI/ObjectIDConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Dwarrowdelf.Client.UI
+{
+	public sealed class ObjectIDConverter : IValueConverter
+	{
+		#region IValueConverter Members
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			var oid = (ObjectID)value;
+
+			if (oid == ObjectID.NullObjectID)
+				return "NULL";
+			else if (oid == ObjectID.AnyObjectID)
+				return "ANY";
+			else
+			{
+				char c;
+				switch (oid.ObjectType)
+				{
+					case Dwarrowdelf.ObjectType.Building: c = 'B'; break;
+					case Dwarrowdelf.ObjectType.Environment: c = 'E'; break;
+					case Dwarrowdelf.ObjectType.Item: c = 'I'; break;
+					case Dwarrowdelf.ObjectType.Living: c = 'L'; break;
+					default: c = '?'; break;
+				}
+
+				return String.Format("{0}{1}", c, oid.Value);
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
---
+++ Client/Client/UI/TreeViewTemplateDictionary.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using AvalonDock;
+using System.Windows.Media;
+
+namespace Dwarrowdelf.Client.UI
+{
+	sealed partial class TreeViewTemplateDictionary
+	{
+		void MenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			var menuItem = (MenuItem)sender;
+			var cm = (ContextMenu)menuItem.Parent;
+			DependencyObject obj = cm.PlacementTarget;
+
+			while (obj != null && !(obj is TreeViewItem))
+				obj = VisualTreeHelper.GetParent(obj);
+
+			var tvi = obj as TreeViewItem;
+			var concreteObject = tvi.Header as ConcreteObject;
+
+			if (concreteObject == null)
+				return;
+
+			var tag = (string)menuItem.Tag;
+
+			switch (tag)
+			{
+				case "Goto":
+					{
+						var movable = concreteObject as MovableObject;
+
+						if (movable == null || movable.Environment == null)
+							return;
+
+						App.MainWindow.MapControl.ScrollTo(movable.Environment, movable.Location);
+					}
+					break;
+
+				case "Info":
+					{
+						var dlg = new ObjectEditDialog();
+						dlg.DataContext = concreteObject;
+						dlg.Owner = App.MainWindow;
+						dlg.Show();
+
+					}
+					break;
+
+				case "Control":
+					{
+						var living = concreteObject as LivingObject;
+
+						var wnd = new LivingControlWindow();
+						wnd.DataContext = living;
+						wnd.Owner = App.MainWindow;
+						wnd.Show();
+					}
+					break;
+
+				default:
+					throw new Exception();
+			}
+		}
+	}
+}
