9ffd8792dd9a17c956b567cbd4632caaa501e844
Tomi Valkeinen <tomba@iki.fi>
2012-01-18 11:12:06 +0200

    terraingen test cleanup

--- Tests/TerrainGenTest/MainWindow.xaml.cs
+++ Tests/TerrainGenTest/MainWindow.xaml.cs
@@ -15,12 +15,11 @@ using Dwarrowdelf;
 
 namespace TerrainGenTest
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		TerrainWriter m_terrain;
+
+		public BitmapSource Bmp { get { return m_terrain.Bmp; } }
 
 		public MainWindow()
 		{
@@ -29,19 +28,11 @@ namespace TerrainGenTest
 			InitializeComponent();
 		}
 
-		protected override void OnInitialized(EventArgs e)
+		protected override void OnSourceInitialized(EventArgs e)
 		{
-			base.OnInitialized(e);
+			base.OnSourceInitialized(e);
 
-			image.Source = m_terrain.Bmp;
-			image.Width = m_terrain.Bmp.PixelWidth;
-			image.Height = m_terrain.Bmp.PixelHeight;
-
-			RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.NearestNeighbor);
-
-			this.PreviewKeyDown += Window_PreKeyDown;
-
-			//slider1.Value = 16;
+			Render();
 		}
 
 		void Render()
@@ -49,112 +40,31 @@ namespace TerrainGenTest
 			if (!this.IsInitialized)
 				return;
 
-			m_terrain.Do(slider3.Value, slider2.Value);
+			double range = rangeSlider.Value;
+			double h = hSlider.Value;
+
+			m_terrain.Generate(range, h);
+
 			timeTextBox.Text = m_terrain.Time.TotalMilliseconds.ToString();
 			minTextBox.Text = m_terrain.Min.ToString();
 			maxTextBox.Text = m_terrain.Max.ToString();
 		}
 
-		protected override void OnSourceInitialized(EventArgs e)
+		private void zoomSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
 		{
-			base.OnSourceInitialized(e);
-
-			Canvas.SetLeft(image, this.Width / 2 - image.Width / 2);
-			Canvas.SetTop(image, this.Height / 2 - image.Height / 2);
-
-			Render();
-		}
-
-		Direction KeyToDir(Key key)
-		{
-			Direction dir;
-
-			switch (key)
-			{
-				case Key.Up: dir = Direction.North; break;
-				case Key.Down: dir = Direction.South; break;
-				case Key.Left: dir = Direction.West; break;
-				case Key.Right: dir = Direction.East; break;
-				case Key.Home: dir = Direction.NorthWest; break;
-				case Key.End: dir = Direction.SouthWest; break;
-				case Key.PageUp: dir = Direction.NorthEast; break;
-				case Key.PageDown: dir = Direction.SouthEast; break;
-				default:
-					throw new Exception();
-			}
-
-			return dir;
-		}
-
-		bool KeyIsDir(Key key)
-		{
-			switch (key)
-			{
-				case Key.Up: break;
-				case Key.Down: break;
-				case Key.Left: break;
-				case Key.Right: break;
-				case Key.Home: break;
-				case Key.End: break;
-				case Key.PageUp: break;
-				case Key.PageDown: break;
-				default:
-					return false;
-			}
-			return true;
-		}
-
-
-		void Window_PreKeyDown(object sender, KeyEventArgs e)
-		{
-#if asd
-			if (KeyIsDir(e.Key))
-			{
-				e.Handled = true;
-				Direction dir = KeyToDir(e.Key);
-
-				var v = IntVector.FromDirection(dir);
-
-				Canvas.SetLeft(image, Canvas.GetLeft(image) - v.X * 128);
-				Canvas.SetTop(image, Canvas.GetTop(image) + v.Y * 128);
-
-				//map.CenterPos += IntVector.FromDirection(dir);
-			}
-			else if (e.Key == Key.Add)
-			{
-				e.Handled = true;
-				slider1.Value *= 2;
-				//map.TileSize += 8;
-			}
-
-			else if (e.Key == Key.Subtract)
-			{
-				e.Handled = true;
-				slider1.Value /= 2;
-				//if (map.TileSize <= 16)
-				//	return;
-				//map.TileSize -= 8;
-			}
-			else if (e.Key == Key.Space)
-			{
-				Render();
-			}
-#endif
-		}
-
-		private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-		{
-			if (m_terrain.Bmp == null)
-				return;
-
-			var v = slider1.Value;
+			var v = zoomSlider.Value;
 
 			image.Width = m_terrain.Bmp.PixelWidth * v;
 			image.Height = m_terrain.Bmp.PixelHeight * v;
 
 		}
 
-		private void slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		private void hSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			Render();
+		}
+
+		private void rangeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
 		{
 			Render();
 		}
--- Tests/TerrainGenTest/TerrainWriter.cs
+++ Tests/TerrainGenTest/TerrainWriter.cs
@@ -7,44 +7,52 @@ using Dwarrowdelf;
 using System.Windows;
 using System.Windows.Media;
 using Dwarrowdelf.TerrainGen;
+using System.Diagnostics;
 
 namespace TerrainGenTest
 {
 	class TerrainWriter
 	{
 		WriteableBitmap m_bmp;
-		Grid2D<double> m_grid;
+		ArrayGrid2D<double> m_grid;
 
-		public WriteableBitmap Bmp { get; private set; }
+		public BitmapSource Bmp { get { return m_bmp; } }
 
 		public TerrainWriter()
 		{
 			int sizeExp = 9;
 			int size = (int)Math.Pow(2, sizeExp) + 1;
-			m_grid = new Grid2D<double>(size, size);
+			m_grid = new ArrayGrid2D<double>(size, size);
 
 			m_bmp = new WriteableBitmap(size, size, 96, 96, PixelFormats.Bgr32, null);
-			this.Bmp = m_bmp;
 		}
 
 		public TimeSpan Time { get; private set; }
 		public double Min { get; private set; }
 		public double Max { get; private set; }
 
-		public void Do(double range = 5, double h = 0.75)
+		public void Generate(double range, double h)
 		{
-			var sw = System.Diagnostics.Stopwatch.StartNew();
+			m_grid.Clear();
+
+			GenerateTerrain(m_grid, range, h);
+
+			RenderTerrain(m_grid);
+		}
+
+		void GenerateTerrain(ArrayGrid2D<double> grid, double range, double h)
+		{
 			double average = 10;
 
-			DiamondSquare.Render(m_grid, average, range, h);
+			DiamondSquare.Render(grid, average, range, h);
 
-			sw.Stop();
-			this.Time = sw.Elapsed;
+			Clamper.Clamp(grid, average);
+		}
 
-			Clamper.Clamp(m_grid, average);
-
+		void RenderTerrain(ArrayGrid2D<double> grid)
+		{
 			double min, max;
-			Clamper.MinMax(m_grid, out min, out max);
+			Clamper.MinMax(grid, out min, out max);
 
 			this.Min = min;
 			this.Max = max;
@@ -52,21 +60,15 @@ namespace TerrainGenTest
 			var diff = max - min;
 			var mul = 255 / diff;
 
-			uint[] array = new uint[m_grid.Width];
+			uint[] array = new uint[grid.Width];
 
 			m_bmp.Lock();
 
-			for (int y = 0; y < m_grid.Height; ++y)
+			for (int y = 0; y < grid.Height && y < m_bmp.PixelHeight; ++y)
 			{
-				if (y >= m_bmp.PixelHeight)
-						continue;
-
-				for (int x = 0; x < m_grid.Width; ++x)
+				for (int x = 0; x < grid.Width && x < m_bmp.PixelWidth; ++x)
 				{
-					if (x >= m_bmp.PixelWidth)
-						continue;
-
-					var v = m_grid[new IntPoint(x, y)];
+					var v = grid[x, y];
 
 					v -= min;
 					v *= mul;
@@ -81,11 +83,12 @@ namespace TerrainGenTest
 					array[x] = (r << 16) | (g << 8) | (b << 0);
 				}
 
-				m_bmp.WritePixels(new Int32Rect(0, y, m_grid.Width, 1), array, m_grid.Width * 4, 0);
+				m_bmp.WritePixels(new Int32Rect(0, y, grid.Width, 1), array, grid.Width * 4, 0);
 			}
 
-			m_bmp.AddDirtyRect(new Int32Rect(0, 0, m_grid.Width, m_grid.Height));
+			m_bmp.AddDirtyRect(new Int32Rect(0, 0, m_bmp.PixelWidth, m_bmp.PixelHeight));
 			m_bmp.Unlock();
 		}
+
 	}
 }
