f6f662d1c38b601a39faeb86a146b20a9fa8234f
Tomi Valkeinen <tomba@iki.fi>
2012-03-07 18:04:53 +0200

    parallel save and load for tiledata

--- Server/World/EnvironmentObject.cs
+++ Server/World/EnvironmentObject.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
-using System.Diagnostics;
-using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace Dwarrowdelf.Server
 {
@@ -713,55 +717,138 @@ namespace Dwarrowdelf.Server
 			{
 				var grid = (TileGrid)value;
 
-				writer.WriteStartArray();
-
 				int w = grid.Grid.GetLength(2);
 				int h = grid.Grid.GetLength(1);
 				int d = grid.Grid.GetLength(0);
 
-				var oldFormatting = writer.Formatting;
-				writer.Formatting = Newtonsoft.Json.Formatting.None;
+				writer.WriteStartObject();
 
+				writer.WritePropertyName("Width");
 				writer.WriteValue(w);
+				writer.WritePropertyName("Height");
 				writer.WriteValue(h);
+				writer.WritePropertyName("Depth");
 				writer.WriteValue(d);
 
-				var srcArr = grid.Grid;
+				writer.WritePropertyName("TileData");
+				writer.WriteStartArray();
 
-				for (int z = 0; z < d; ++z)
-					for (int y = 0; y < h; ++y)
-						for (int x = 0; x < w; ++x)
-							writer.WriteValue(srcArr[z, y, x].Raw);
+				var queue = new BlockingCollection<Tuple<int, byte[]>>();
 
-				writer.Formatting = oldFormatting;
+				var writerTask = Task.Factory.StartNew(() =>
+				{
+					foreach (var tuple in queue.GetConsumingEnumerable())
+					{
+						writer.WriteValue(tuple.Item1);
+						writer.WriteValue(tuple.Item2);
+					}
+				});
+
+				Parallel.For(0, d, z =>
+				{
+					using (var memStream = new MemoryStream())
+					{
+						using (var compressStream = new DeflateStream(memStream, CompressionMode.Compress, true))
+						using (var bufferedStream = new BufferedStream(compressStream))
+						using (var streamWriter = new BinaryWriter(bufferedStream))
+						{
+							var srcArr = grid.Grid;
+
+							for (int y = 0; y < h; ++y)
+								for (int x = 0; x < w; ++x)
+									streamWriter.Write(srcArr[z, y, x].Raw);
+						}
+
+						queue.Add(new Tuple<int, byte[]>(z, memStream.ToArray()));
+					}
+				});
+
+				queue.CompleteAdding();
+
+				writerTask.Wait();
 
 				writer.WriteEndArray();
+				writer.WriteEndObject();
+			}
+
+			static void ReadAndValidate(Newtonsoft.Json.JsonReader reader, Newtonsoft.Json.JsonToken token)
+			{
+				reader.Read();
+				if (reader.TokenType != token)
+					throw new Exception();
+			}
+
+			static int ReadIntProperty(Newtonsoft.Json.JsonReader reader, string propertyName)
+			{
+				reader.Read();
+				if (reader.TokenType != Newtonsoft.Json.JsonToken.PropertyName || (string)reader.Value != propertyName)
+					throw new Exception();
+
+				reader.Read();
+				if (reader.TokenType != Newtonsoft.Json.JsonToken.Integer)
+					throw new Exception();
+
+				return (int)(long)reader.Value;
 			}
 
 			public object Read(Newtonsoft.Json.JsonReader reader)
 			{
-				Debug.Assert(reader.TokenType == Newtonsoft.Json.JsonToken.StartArray);
+				if (reader.TokenType != Newtonsoft.Json.JsonToken.StartObject)
+					throw new Exception();
 
-				reader.Read();
-				int w = (int)(long)reader.Value;
-				reader.Read();
-				int h = (int)(long)reader.Value;
-				reader.Read();
-				int d = (int)(long)reader.Value;
+				int w = ReadIntProperty(reader, "Width");
+				int h = ReadIntProperty(reader, "Height");
+				int d = ReadIntProperty(reader, "Depth");
 
 				var grid = new TileGrid(new IntSize3(w, h, d));
 				var dstArr = grid.Grid;
 
-				for (int z = 0; z < d; ++z)
-					for (int y = 0; y < h; ++y)
-						for (int x = 0; x < w; ++x)
-						{
-							reader.Read();
-							dstArr[z, y, x].Raw = (ulong)(long)reader.Value;
-						}
-
 				reader.Read();
-				Debug.Assert(reader.TokenType == Newtonsoft.Json.JsonToken.EndArray);
+				if (reader.TokenType != Newtonsoft.Json.JsonToken.PropertyName || (string)reader.Value != "TileData")
+					throw new Exception();
+
+				ReadAndValidate(reader, Newtonsoft.Json.JsonToken.StartArray);
+
+				var queue = new BlockingCollection<Tuple<int, byte[]>>();
+
+				var readerTask = Task.Factory.StartNew(() =>
+				{
+					for (int i = 0; i < d; ++i)
+					{
+						reader.Read();
+						int z = (int)(long)reader.Value;
+
+						byte[] buf = reader.ReadAsBytes();
+
+						queue.Add(new Tuple<int, byte[]>(z, buf));
+					}
+
+					queue.CompleteAdding();
+				});
+
+				Parallel.For(0, d, i =>
+				{
+					var tuple = queue.Take();
+
+					int z = tuple.Item1;
+					byte[] arr = tuple.Item2;
+
+					using (var memStream = new MemoryStream(arr))
+					{
+						using (var decompressStream = new DeflateStream(memStream, CompressionMode.Decompress))
+						using (var streamReader = new BinaryReader(decompressStream))
+						{
+							for (int y = 0; y < h; ++y)
+								for (int x = 0; x < w; ++x)
+									dstArr[z, y, x].Raw = streamReader.ReadUInt64();
+						}
+					}
+				});
+
+				readerTask.Wait();
+
+				ReadAndValidate(reader, Newtonsoft.Json.JsonToken.EndArray);
+				ReadAndValidate(reader, Newtonsoft.Json.JsonToken.EndObject);
 
 				return grid;
 			}
