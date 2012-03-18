b95c8038375798ce03894b51fe89993bf902095a
Tomi Valkeinen <tomba@iki.fi>
2012-03-18 17:32:14 +0200

    use BufferedStreams inside GameNetStream

--- Common/Net/GameNetStream.cs
+++ Common/Net/GameNetStream.cs
@@ -12,66 +12,37 @@ namespace Dwarrowdelf
 	{
 		Socket m_socket;
 
-		byte[] m_receiveBuffer;
-		int m_received;
-		int m_read;
 		int m_totalRead;
-
-		byte[] m_sendBuffer;
-		int m_used;
 		int m_totalSent;
+
+		BufferedStream m_recvStream;
+		BufferedStream m_sendStream;
 
 		public GameNetStream(Socket socket, int receiveBufferLen, int sendBufferLen)
 		{
 			m_socket = socket;
 
-			m_receiveBuffer = new byte[receiveBufferLen];
-			m_sendBuffer = new byte[sendBufferLen];
+			var netStream = new NetworkStream(socket, false);
+			m_recvStream = new BufferedStream(netStream, receiveBufferLen);
+			m_sendStream = new BufferedStream(netStream, sendBufferLen);
 		}
 
 		public override bool CanRead { get { return true; } }
-		public override bool CanSeek { get { return false; } }
 		public override bool CanWrite { get { return true; } }
+		public override bool CanSeek { get { return false; } }
 
 		public int ReadBytes { get { return m_totalRead; } }
 		public int SentBytes { get { return m_totalSent; } }
 
 		public override int ReadByte()
 		{
-			if (m_received == m_read)
-			{
-				int len = m_socket.Receive(m_receiveBuffer);
-
-				if (len == 0)
-				{
-					throw new SocketException();
-				}
-
-				m_received = len;
-				m_read = 0;
-			}
-
 			m_totalRead++;
-			return m_receiveBuffer[m_read++];
+			return m_recvStream.ReadByte();
 		}
 
 		public override int Read(byte[] buffer, int offset, int count)
 		{
-			int len;
-
-			if (m_received == m_read)
-			{
-				len = m_socket.Receive(buffer, offset, count, SocketFlags.None);
-			}
-			else
-			{
-				len = Math.Min(count, m_received - m_read);
-
-				Buffer.BlockCopy(m_receiveBuffer, m_read, buffer, offset, len);
-
-				m_read += len;
-			}
-
+			var len = m_recvStream.Read(buffer, offset, count);
 			m_totalRead += len;
 			return len;
 		}
@@ -79,33 +50,19 @@ namespace Dwarrowdelf
 
 		public override void Flush()
 		{
-			int len = m_socket.Send(m_sendBuffer, m_used, SocketFlags.None);
-			if (len != m_used)
-				throw new Exception("short write");
-
-			m_totalSent += len;
-
-			m_used = 0;
+			m_sendStream.Flush();
 		}
 
 		public override void WriteByte(byte value)
 		{
-			if (m_used == m_sendBuffer.Length)
-				Flush();
-
-			m_sendBuffer[m_used++] = value;
+			m_totalSent++;
+			m_sendStream.WriteByte(value);
 		}
 
 		public override void Write(byte[] buffer, int offset, int count)
 		{
-			if (m_used > 0)
-				Flush();
-
-			int len = m_socket.Send(buffer, 0, count, SocketFlags.None);
-			if (len != count)
-				throw new Exception("short write");
-
-			m_totalSent += len;
+			m_totalSent += count;
+			m_sendStream.Write(buffer, offset, count);
 		}
 
 
