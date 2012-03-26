bceb1ea1d5095dcd17cc2ff9e13120a12a270489
Tomi Valkeinen <tomba@iki.fi>
2012-03-26 21:31:36 +0300

    add LoaderOptimization.MultiDomain

---
+++ Client/Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dwarrowdelf.Client
+{
+	static class Program
+	{
+		[STAThread]
+		[LoaderOptimization(LoaderOptimization.MultiDomain)]
+		public static void Main()
+		{
+			var app = new App();
+			app.InitializeComponent();
+			app.Run();
+		}
+	}
+}
