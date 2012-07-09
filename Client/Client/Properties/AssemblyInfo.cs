952ce78b0adb4730d5d8a9870d3981320b49667a
Tomi Valkeinen <tomba@iki.fi>
2012-07-09 22:19:11 +0300

    cleanup project files

--- Client/Client/Properties/AssemblyInfo.cs
+++ Client/Client/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WPFClient")]
+[assembly: AssemblyTitle("Dwarrowdelf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WPFClient")]
+[assembly: AssemblyProduct("Dwarrowdelf")]
 [assembly: AssemblyCopyright("Copyright © Tomi Valkeinen 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
