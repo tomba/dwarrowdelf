a1f3ef5e07e17a8b4c8cdbccb1a85bd1fbbfdb47
Tomi Valkeinen <tomba@iki.fi>
2012-12-11 20:44:59 +0200

    split world ob handling

--- Server/World/World/World.cs
+++ Server/World/World/World.cs
@@ -26,11 +26,6 @@ namespace Dwarrowdelf.Server
 
 		ReaderWriterLockSlim m_rwLock = new ReaderWriterLockSlim();
 
-		[SaveGameProperty]
-		Dictionary<ObjectID, BaseObject> m_objectMap;
-		[SaveGameProperty]
-		int[] m_objectIDcounterArray;
-
 		public event Action<Change> WorldChanged;
 		public event Action<GameReport> ReportReceived;
 
@@ -146,75 +141,5 @@ namespace Dwarrowdelf.Server
 			if (ReportReceived != null)
 				ReportReceived(report);
 		}
-
-		internal void AddGameObject(BaseObject ob)
-		{
-			if (ob.ObjectID == ObjectID.NullObjectID)
-				throw new ArgumentException("Null ObjectID");
-
-			lock (m_objectMap)
-				m_objectMap.Add(ob.ObjectID, ob);
-		}
-
-		internal void RemoveGameObject(BaseObject ob)
-		{
-			if (ob.ObjectID == ObjectID.NullObjectID)
-				throw new ArgumentException("Null ObjectID");
-
-			lock (m_objectMap)
-				if (m_objectMap.Remove(ob.ObjectID) == false)
-					throw new Exception();
-		}
-
-		public BaseObject FindObject(ObjectID objectID)
-		{
-			if (objectID == ObjectID.NullObjectID)
-				throw new ArgumentException("Null ObjectID");
-
-
-			lock (m_objectMap)
-			{
-				BaseObject ob;
-				return m_objectMap.TryGetValue(objectID, out ob) ? ob : null;
-			}
-		}
-
-		public T FindObject<T>(ObjectID objectID) where T : BaseObject
-		{
-			var ob = FindObject(objectID);
-
-			if (ob == null)
-				return null;
-
-			return (T)ob;
-		}
-
-
-		public BaseObject GetObject(ObjectID objectID)
-		{
-			var ob = FindObject(objectID);
-
-			if (ob == null)
-				throw new Exception();
-
-			return ob;
-		}
-
-		public T GetObject<T>(ObjectID objectID) where T : BaseObject
-		{
-			return (T)GetObject(objectID);
-		}
-
-		internal ObjectID GetNewObjectID(ObjectType objectType)
-		{
-			// XXX overflows
-			//return new ObjectID(objectType, Interlocked.Increment(ref m_objectIDcounterArray[(int)objectType]));
-			// XXX use a common counter to make debugging simpler
-			// XXX check wrapping and int -> uint
-			return new ObjectID(objectType, (uint)Interlocked.Increment(ref m_objectIDcounterArray[0]));
-		}
-
-		public IEnumerable<BaseObject> AllObjects { get { return m_objectMap.Values.AsEnumerable(); } }
-
 	}
 }
