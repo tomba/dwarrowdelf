84737ff2e15b197b39698bea6ca8264e9849b869
Tomi Valkeinen <tomba@iki.fi>
2013-01-11 18:25:01 +0200

    cleanup & fix EnumBitMasks

--- Common/DataTypes/EnumBitMasks.cs
+++ Common/DataTypes/EnumBitMasks.cs
@@ -10,13 +10,15 @@ namespace Dwarrowdelf
 	[Serializable]
 	public class EnumBitMask32<TEnum>
 	{
+		static int s_numBits;
 		uint m_mask;
 
 		static EnumBitMask32()
 		{
-			var max = EnumHelpers.GetEnumMax<TEnum>() + 1;
+			var max = EnumHelpers.GetEnumMax<TEnum>();
 			if (max > 32)
 				throw new Exception();
+			s_numBits = max;
 		}
 
 		public EnumBitMask32()
@@ -26,7 +28,10 @@ namespace Dwarrowdelf
 
 		public EnumBitMask32(TEnum enumValue)
 		{
-			m_mask = 1U << EnumConv.ToInt32(enumValue);
+			if (EnumConv.ToInt32(enumValue) != 0)
+				m_mask = EnumToBit(enumValue);
+			else
+				m_mask = 0;
 		}
 
 		public EnumBitMask32(IEnumerable<TEnum> enumValues)
@@ -36,7 +41,7 @@ namespace Dwarrowdelf
 			if (enumValues != null)
 			{
 				foreach (TEnum e in enumValues)
-					mask |= 1U << EnumConv.ToInt32(e);
+					mask |= EnumToBit(e);
 			}
 
 			m_mask = mask;
@@ -44,38 +49,66 @@ namespace Dwarrowdelf
 
 		public bool Get(TEnum enumValue)
 		{
-			return m_mask == 0 || (m_mask & (1U << EnumConv.ToInt32(enumValue))) != 0;
+			return m_mask == 0 || (m_mask & EnumToBit(enumValue)) != 0;
 		}
 
 		public IEnumerable<TEnum> EnumValues
 		{
 			get
 			{
-				for (int i = 0; i < 32; ++i)
+				for (int i = 0; i < s_numBits; ++i)
 				{
-					var b = ((m_mask >> i) & 1) == 1;
-					if (b)
-						yield return EnumConv.ToEnum<TEnum>(i);
+					if (m_mask == 0 || (m_mask & (1U << i)) != 0)
+						yield return EnumConv.ToEnum<TEnum>(i + 1);
 				}
 			}
 		}
 
+		uint EnumToBit(TEnum e)
+		{
+			return 1U << (EnumConv.ToInt32(e) - 1);
+		}
+
 		public override string ToString()
 		{
-			return m_mask.ToString();
+			if (m_mask == 0)
+				return "null";
+
+			var sb = new StringBuilder("0b", s_numBits + 2);
+
+			bool zeroes = true;
+
+			for (int i = s_numBits - 1; i >= 0; --i)
+			{
+				bool b = (m_mask & (1U << i)) != 0;
+
+				if (zeroes)
+				{
+					if (!b)
+						continue;
+
+					zeroes = false;
+				}
+
+				sb.Append(b ? '1' : '0');
+			}
+
+			return sb.ToString();
 		}
 	}
 
 	[Serializable]
 	public class EnumBitMask64<TEnum>
 	{
+		static int s_numBits;
 		ulong m_mask;
 
 		static EnumBitMask64()
 		{
-			var max = EnumHelpers.GetEnumMax<TEnum>() + 1;
+			var max = EnumHelpers.GetEnumMax<TEnum>();
 			if (max > 64)
 				throw new Exception();
+			s_numBits = max;
 		}
 
 		public EnumBitMask64()
@@ -85,7 +118,10 @@ namespace Dwarrowdelf
 
 		public EnumBitMask64(TEnum enumValue)
 		{
-			m_mask = 1UL << EnumConv.ToInt32(enumValue);
+			if (EnumConv.ToInt32(enumValue) != 0)
+				m_mask = EnumToBit(enumValue);
+			else
+				m_mask = 0;
 		}
 
 		public EnumBitMask64(IEnumerable<TEnum> enumValues)
@@ -95,7 +131,7 @@ namespace Dwarrowdelf
 			if (enumValues != null)
 			{
 				foreach (TEnum e in enumValues)
-					mask |= 1UL << EnumConv.ToInt32(e);
+					mask |= EnumToBit(e);
 			}
 
 			m_mask = mask;
@@ -103,42 +139,79 @@ namespace Dwarrowdelf
 
 		public bool Get(TEnum enumValue)
 		{
-			return m_mask == 0 || (m_mask & (1UL << EnumConv.ToInt32(enumValue))) != 0;
+			return m_mask == 0 || (m_mask & EnumToBit(enumValue)) != 0;
 		}
 
 		public IEnumerable<TEnum> EnumValues
 		{
 			get
 			{
-				for (int i = 0; i < 32; ++i)
+				for (int i = 0; i < s_numBits; ++i)
 				{
-					var b = ((m_mask >> i) & 1) == 1;
-					if (b)
-						yield return EnumConv.ToEnum<TEnum>(i);
+					if (m_mask == 0 || (m_mask & (1UL << i)) != 0)
+						yield return EnumConv.ToEnum<TEnum>(i + 1);
 				}
 			}
 		}
 
+		ulong EnumToBit(TEnum e)
+		{
+			return 1UL << (EnumConv.ToInt32(e) - 1);
+		}
+
 		public override string ToString()
 		{
-			return m_mask.ToString();
+			if (m_mask == 0)
+				return "null";
+
+			var sb = new StringBuilder("0b", s_numBits + 2);
+
+			bool zeroes = true;
+
+			for (int i = s_numBits - 1; i >= 0; --i)
+			{
+				bool b = (m_mask & (1UL << i)) != 0;
+
+				if (zeroes)
+				{
+					if (!b)
+						continue;
+
+					zeroes = false;
+				}
+
+				sb.Append(b ? '1' : '0');
+			}
+
+			return sb.ToString();
 		}
 	}
 
 	[Serializable]
 	public class EnumBitMask<TEnum>
 	{
+		static int s_numBits;
 		BitArray m_mask;
+
+		static EnumBitMask()
+		{
+			s_numBits = EnumHelpers.GetEnumMax<TEnum>();
+		}
 
 		public EnumBitMask()
 		{
-			m_mask = new BitArray(EnumHelpers.GetEnumMax<TEnum>() + 1);
+			m_mask = null;
 		}
 
 		public EnumBitMask(TEnum enumValue)
 		{
-			BitArray mask = new BitArray(EnumHelpers.GetEnumMax<TEnum>() + 1);
-			mask.Set(EnumConv.ToInt32(enumValue), true);
+			BitArray mask = null;
+
+			if (EnumConv.ToInt32(enumValue) != 0)
+			{
+				mask = new BitArray(s_numBits);
+				mask.Set(EnumConv.ToInt32(enumValue) - 1, true);
+			}
 
 			m_mask = mask;
 		}
@@ -149,9 +222,9 @@ namespace Dwarrowdelf
 
 			if (enumValues != null && enumValues.Any())
 			{
-				mask = new BitArray(EnumHelpers.GetEnumMax<TEnum>() + 1);
+				mask = new BitArray(s_numBits);
 				foreach (TEnum e in enumValues)
-					mask.Set(EnumConv.ToInt32(e), true);
+					mask.Set(EnumConv.ToInt32(e) - 1, true);
 			}
 
 			m_mask = mask;
@@ -159,18 +232,17 @@ namespace Dwarrowdelf
 
 		public bool Get(TEnum enumValue)
 		{
-			return m_mask == null || m_mask.Get(EnumConv.ToInt32(enumValue));
+			return m_mask == null || m_mask.Get(EnumConv.ToInt32(enumValue) - 1);
 		}
 
 		public IEnumerable<TEnum> EnumValues
 		{
 			get
 			{
-				for (int i = 0; i < m_mask.Length; ++i)
+				for (int i = 0; i < s_numBits; ++i)
 				{
-					var b = m_mask.Get(i);
-					if (b)
-						yield return EnumConv.ToEnum<TEnum>(i);
+					if (m_mask == null || m_mask.Get(i))
+						yield return EnumConv.ToEnum<TEnum>(i + 1);
 				}
 			}
 		}
@@ -180,10 +252,24 @@ namespace Dwarrowdelf
 			if (m_mask == null)
 				return "null";
 
-			StringBuilder sb = new StringBuilder(m_mask.Count);
+			var sb = new StringBuilder("0b", s_numBits + 2);
 
-			foreach (bool b in m_mask)
+			bool zeroes = true;
+
+			for (int i = s_numBits - 1; i >= 0; --i)
+			{
+				bool b = m_mask.Get(i);
+
+				if (zeroes)
+				{
+					if (!b)
+						continue;
+
+					zeroes = false;
+				}
+
 				sb.Append(b ? '1' : '0');
+			}
 
 			return sb.ToString();
 		}
