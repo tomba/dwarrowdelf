5dcd5bde38742fd51fd1e319139d9db2489691df
Tomi Valkeinen <tomba@iki.fi>
2013-02-13 22:33:28 +0200

    add bresenhams line algos

---
+++ Common/Lib/LOS/Bresenhams.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Dwarrowdelf
+{
+	public class Bresenhams
+	{
+		static void Swap<T>(ref T lhs, ref T rhs) { T temp; temp = lhs; lhs = rhs; rhs = temp; }
+
+		/// <summary>
+		/// Plot the line from src to dst
+		/// </summary>
+		/// <param name="plot">The plotting function (if this returns false, the algorithm stops early)</param>
+		public static void PlotLine(IntPoint2 src, IntPoint2 dst, Func<IntPoint2, bool> plot)
+		{
+			int x0 = src.X;
+			int y0 = src.Y;
+			int x1 = dst.X;
+			int y1 = dst.Y;
+
+			bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+
+			if (steep)
+			{
+				Swap<int>(ref x0, ref y0);
+				Swap<int>(ref x1, ref y1);
+			}
+
+			if (x0 > x1)
+			{
+				Swap<int>(ref x0, ref x1);
+				Swap<int>(ref y0, ref y1);
+			}
+
+			int dX = x1 - x0;
+			int dY = Math.Abs(y1 - y0);
+			int err = dX / 2;
+			int ystep = y0 < y1 ? 1 : -1;
+			int y = y0;
+
+			for (int x = x0; x <= x1; ++x)
+			{
+				IntPoint2 p = steep ? new IntPoint2(y, x) : new IntPoint2(x, y);
+
+				if (!plot(p))
+					return;
+
+				err = err - dY;
+				if (err < 0)
+				{
+					y += ystep;
+					err += dX;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Plot the line from src to dst
+		/// </summary>
+		/// <param name="plot">The plotting function (if this returns false, the algorithm stops early)</param>
+		public static void PlotLine3(IntPoint3 src, IntPoint3 dst, Func<IntPoint3, bool> plot)
+		{
+			int x0 = src.X;
+			int y0 = src.Y;
+			int z0 = src.Z;
+			int x1 = dst.X;
+			int y1 = dst.Y;
+			int z1 = dst.Z;
+
+			bool steepXY = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+			if (steepXY)
+			{
+				Swap(ref x0, ref y0);
+				Swap(ref x1, ref y1);
+			}
+
+			bool steepXZ = Math.Abs(z1 - z0) > Math.Abs(x1 - x0);
+			if (steepXZ)
+			{
+				Swap(ref x0, ref z0);
+				Swap(ref x1, ref z1);
+			}
+
+			int deltaX = Math.Abs(x1 - x0);
+			int deltaY = Math.Abs(y1 - y0);
+			int deltaZ = Math.Abs(z1 - z0);
+
+			int errorXY = deltaX / 2;
+			int errorXZ = deltaX / 2;
+
+			int stepX = (x0 > x1) ? -1 : 1;
+			int stepY = (y0 > y1) ? -1 : 1;
+			int stepZ = (z0 > z1) ? -1 : 1;
+
+			int y = y0;
+			int z = z0;
+
+			for (int x = x0; x <= x1; x += stepX)
+			{
+				int tx = x, ty = y, tz = z;
+
+				if (steepXZ)
+					Swap(ref tx, ref tz);
+				if (steepXY)
+					Swap(ref tx, ref ty);
+
+				if (plot(new IntPoint3(tx, ty, tz)) == false)
+					return;
+
+				errorXY -= deltaY;
+				errorXZ -= deltaZ;
+
+				if (errorXY < 0)
+				{
+					y += stepY;
+					errorXY += deltaX;
+				}
+
+				if (errorXZ < 0)
+				{
+					z += stepZ;
+					errorXZ += deltaX;
+				}
+			}
+		}
+	}
+}
--- Common/Lib/LOS/RayCast.cs
+++ Common/Lib/LOS/RayCast.cs
@@ -8,29 +8,6 @@ namespace Dwarrowdelf
 {
 	public static class RayCast
 	{
-		static bool FindLos(IntPoint2 src, IntPoint2 dst, Func<IntPoint2, bool> blockerDelegate)
-		{
-			bool vis = true;
-
-			Line(src.X, src.Y, dst.X, dst.Y, (x, y) =>
-				{
-					var p = new IntPoint2(x, y);
-
-					if (p == dst)
-						return true;
-
-					if (blockerDelegate(p) == true)
-					{
-						vis = false;
-						return false;
-					}
-
-					return true;
-				});
-
-			return vis;
-		}
-
 		public static void Calculate(IntPoint2 viewerLocation, int visionRange, Grid2D<bool> visibilityMap, IntSize2 mapSize,
 			Func<IntPoint2, bool> blockerDelegate)
 		{
@@ -57,38 +34,25 @@ namespace Dwarrowdelf
 			}
 		}
 
-		private static void Swap<T>(ref T lhs, ref T rhs) { T temp; temp = lhs; lhs = rhs; rhs = temp; }
-
-		/// <summary>
-		/// The plot function delegate
-		/// </summary>
-		/// <param name="x">The x co-ord being plotted</param>
-		/// <param name="y">The y co-ord being plotted</param>
-		/// <returns>True to continue, false to stop the algorithm</returns>
-		public delegate bool PlotFunction(int x, int y);
-
-		/// <summary>
-		/// Plot the line from (x0, y0) to (x1, y10
-		/// </summary>
-		/// <param name="x0">The start x</param>
-		/// <param name="y0">The start y</param>
-		/// <param name="x1">The end x</param>
-		/// <param name="y1">The end y</param>
-		/// <param name="plot">The plotting function (if this returns false, the algorithm stops early)</param>
-		public static void Line(int x0, int y0, int x1, int y1, PlotFunction plot)
+		static bool FindLos(IntPoint2 src, IntPoint2 dst, Func<IntPoint2, bool> blockerDelegate)
 		{
-			bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
-			if (steep) { Swap<int>(ref x0, ref y0); Swap<int>(ref x1, ref y1); }
-			if (x0 > x1) { Swap<int>(ref x0, ref x1); Swap<int>(ref y0, ref y1); }
-			int dX = (x1 - x0), dY = Math.Abs(y1 - y0), err = (dX / 2), ystep = (y0 < y1 ? 1 : -1), y = y0;
+			bool vis = true;
 
-			for (int x = x0; x <= x1; ++x)
+			Bresenhams.PlotLine(src, dst, (p) =>
 			{
-				if (!(steep ? plot(y, x) : plot(x, y)))
-					return;
-				err = err - dY;
-				if (err < 0) { y += ystep; err += dX; }
-			}
+				if (p == dst)
+					return true;
+
+				if (blockerDelegate(p) == true)
+				{
+					vis = false;
+					return false;
+				}
+
+				return true;
+			});
+
+			return vis;
 		}
 	}
 }
