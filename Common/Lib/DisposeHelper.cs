5f3acc680086343ffa415f7a8d24bcc5a94afea3
Tomi Valkeinen <tomba@iki.fi>
2013-03-09 15:11:17 +0200

    DisposeHelper

---
+++ Common/Lib/DisposeHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dwarrowdelf
+{
+	public static class DH
+	{
+		public static void Dispose<T>(ref T v) where T : class, IDisposable
+		{
+			if (v != null)
+			{
+				v.Dispose();
+				v = null;
+			}
+		}
+	}
+}
