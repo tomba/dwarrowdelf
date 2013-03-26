e8d235f388164d6308f1d1f698eeb829d6126e33
Tomi Valkeinen <tomba@iki.fi>
2013-03-26 21:32:17 +0200

    Add HashCodeTest

---
+++ Tests/HashCodeTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using Dwarrowdelf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var map = new Dictionary<int, uint>();
+			int tot = 0;
+			foreach (var p in new IntGrid3(-100, -100, -100, 400, 200, 200).Range())
+			{
+				var h = p.GetHashCode();
+
+				uint c;
+				if (map.TryGetValue(h, out c))
+					map[h] = c + 1;
+				else
+					map[h] = 1;
+
+				tot++;
+			}
+
+			Console.WriteLine("tot {0}, distinct {1}", tot, map.Count);
+		}
+	}
+}
