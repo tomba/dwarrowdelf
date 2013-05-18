93ef381aac66b375013b708432316eb2d90b0c35
Tomi Valkeinen <tomba@iki.fi>
2013-05-18 18:09:54 +0300

    Add LRU

---
+++ Common/Lib/LRU.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dwarrowdelf
+{
+	public class LRUCache<TKey, TValue>
+	{
+		int m_capacity;
+		Dictionary<TKey, LinkedListNode<LRUCacheItem>> m_cacheMap;
+		LinkedList<LRUCacheItem> m_lruList;
+
+		public LRUCache(int capacity)
+		{
+			m_capacity = capacity;
+			m_cacheMap = new Dictionary<TKey, LinkedListNode<LRUCacheItem>>(capacity);
+			m_lruList = new LinkedList<LRUCacheItem>();
+		}
+
+		public bool TryGet(TKey key, out TValue value)
+		{
+			LinkedListNode<LRUCacheItem> node;
+
+			if (m_cacheMap.TryGetValue(key, out node))
+			{
+				value = node.Value.Value;
+				m_lruList.Remove(node);
+				m_lruList.AddLast(node);
+				return true;
+			}
+			else
+			{
+				value = default(TValue);
+				return false;
+			}
+		}
+
+		public void Add(TKey key, TValue val)
+		{
+			if (m_cacheMap.Count >= m_capacity)
+			{
+				var first = m_lruList.First;
+				m_lruList.RemoveFirst();
+				m_cacheMap.Remove(first.Value.Key);
+			}
+
+			var cacheItem = new LRUCacheItem(key, val);
+			var node = new LinkedListNode<LRUCacheItem>(cacheItem);
+			m_lruList.AddLast(node);
+			m_cacheMap.Add(key, node);
+		}
+
+		class LRUCacheItem
+		{
+			public TKey Key;
+			public TValue Value;
+
+			public LRUCacheItem(TKey k, TValue v)
+			{
+				this.Key = k;
+				this.Value = v;
+			}
+		}
+	}
+}
