e916fc8ae97e81922572171aab9311251867eae0
Tomi Valkeinen <tomba@iki.fi>
2013-08-12 22:39:31 +0300

    move stuff to gamedata

--- Client/Client/App.xaml.cs
+++ Client/Client/App.xaml.cs
@@ -28,46 +28,20 @@ namespace Dwarrowdelf.Client
 
 		async void App_Startup(object sender, StartupEventArgs e)
 		{
-			GameData.Data = new GameData();
-
-			GameData.Data.ConnectManager.UserConnected += ConnectManager_UserConnected;
-
 			if (e.Args.Contains("adventure"))
 				ClientConfig.NewGameMode = GameMode.Adventure;
 			else if (e.Args.Contains("fortress"))
 				ClientConfig.NewGameMode = GameMode.Fortress;
 
+			GameData.InitGameData();
+			
 			var mainWindow = new UI.MainWindow();
 			this.MainWindow = mainWindow;
 			mainWindow.Show();
 
 			if (ClientConfig.AutoConnect)
 			{
-				var dlg = mainWindow.OpenLogOnDialog();
-
-				try
-				{
-					var prog = new Progress<string>(str => dlg.AppendText(str));
-
-					var path = Win32.SavedGamesFolder.GetSavedGamesPath();
-					path = System.IO.Path.Combine(path, "Dwarrowdelf", "save");
-
-					var options = new EmbeddedServerOptions()
-					{
-						ServerMode = ClientConfig.EmbeddedServerMode,
-						NewGameMode = ClientConfig.NewGameMode,
-						SaveGamePath = path,
-						CleanSaveDir = ClientConfig.CleanSaveDir,
-					};
-
-					await GameData.Data.ConnectManager.StartServerAndConnectAsync(options, ClientConfig.ConnectionType, prog);
-				}
-				catch (Exception exc)
-				{
-					MessageBox.Show(this.MainWindow, exc.ToString(), "Failed to autoconnect");
-				}
-
-				dlg.Close();
+				await GameData.Data.StartServerAndConnectAsync();
 			}
 		}
 
@@ -84,47 +58,6 @@ namespace Dwarrowdelf.Client
 		{
 			// continue server?
 		}
-
-
-		void ConnectManager_UserConnected(ClientUser user)
-		{
-			if (GameData.Data.User != null)
-				throw new Exception();
-
-			GameData.Data.User = user;
-			GameData.Data.World = user.World;
-
-			user.DisconnectEvent += user_DisconnectEvent;
-
-			var controllable = GameData.Data.World.Controllables.FirstOrDefault();
-			if (controllable != null && controllable.Environment != null)
-			{
-				var mapControl = App.GameWindow.MapControl;
-				mapControl.IsVisibilityCheckEnabled = !user.IsSeeAll;
-				mapControl.Environment = controllable.Environment;
-				mapControl.CenterPos = new Point(controllable.Location.X, controllable.Location.Y);
-				mapControl.Z = controllable.Location.Z;
-
-				if (GameData.Data.World.GameMode == GameMode.Adventure)
-					App.GameWindow.FocusedObject = controllable;
-			}
-
-			if (Program.StartupStopwatch != null)
-			{
-				Program.StartupStopwatch.Stop();
-				Trace.WriteLine(String.Format("Startup {0} ms", Program.StartupStopwatch.ElapsedMilliseconds));
-				Program.StartupStopwatch = null;
-			}
-		}
-
-		void user_DisconnectEvent()
-		{
-			App.GameWindow.FocusedObject = null;
-			App.GameWindow.MapControl.Environment = null;
-			GameData.Data.User = null;
-			GameData.Data.World = null;
-		}
-
 
 
 
--- Client/Client/GameData.cs
+++ Client/Client/GameData.cs
@@ -8,14 +8,23 @@ using System.Windows;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Threading;
+using System.Threading.Tasks;
 
 namespace Dwarrowdelf.Client
 {
 	sealed class GameData : INotifyPropertyChanged
 	{
-		public static GameData Data;
+		public static GameData Data { get; private set; }
 
-		public GameData()
+		public static void InitGameData()
+		{
+			if (Data != null)
+				throw new Exception();
+
+			Data = new GameData();
+		}
+
+		GameData()
 		{
 			this.TileSet = new TileSet(new Uri("/Dwarrowdelf.Client;component/TileSet/TileSet.png", UriKind.Relative));
 
@@ -24,6 +33,7 @@ namespace Dwarrowdelf.Client
 			m_timer.Tick += delegate { if (_Blink != null) _Blink(); MainWindow.MapControl.InvalidateTileData(); };
 
 			this.ConnectManager = new ConnectManager();
+			this.ConnectManager.UserConnected += ConnectManager_UserConnected;
 		}
 
 		public ConnectManager ConnectManager { get; private set; }
@@ -103,6 +113,78 @@ namespace Dwarrowdelf.Client
 				Notify("IsAutoAdvanceTurn");
 			}
 		}
+
+		public async Task StartServerAndConnectAsync()
+		{
+			var dlg = this.MainWindow.OpenLogOnDialog();
+
+			try
+			{
+				var prog = new Progress<string>(str => dlg.AppendText(str));
+
+				var path = Win32.SavedGamesFolder.GetSavedGamesPath();
+				path = System.IO.Path.Combine(path, "Dwarrowdelf", "save");
+
+				var options = new EmbeddedServerOptions()
+				{
+					ServerMode = ClientConfig.EmbeddedServerMode,
+					NewGameMode = ClientConfig.NewGameMode,
+					SaveGamePath = path,
+					CleanSaveDir = ClientConfig.CleanSaveDir,
+				};
+
+				await this.ConnectManager.StartServerAndConnectAsync(options, ClientConfig.ConnectionType, prog);
+			}
+			catch (Exception exc)
+			{
+				MessageBox.Show(this.MainWindow, exc.ToString(), "Failed to autoconnect");
+			}
+
+			dlg.Close();
+		}
+
+		void ConnectManager_UserConnected(ClientUser user)
+		{
+			if (this.User != null)
+				throw new Exception();
+
+			this.User = user;
+			this.World = user.World;
+
+			user.DisconnectEvent += user_DisconnectEvent;
+
+			var controllable = this.World.Controllables.FirstOrDefault();
+			if (controllable != null && controllable.Environment != null)
+			{
+				var mapControl = App.GameWindow.MapControl;
+				mapControl.IsVisibilityCheckEnabled = !user.IsSeeAll;
+				mapControl.Environment = controllable.Environment;
+				mapControl.CenterPos = new Point(controllable.Location.X, controllable.Location.Y);
+				mapControl.Z = controllable.Location.Z;
+
+				if (this.World.GameMode == GameMode.Adventure)
+					App.GameWindow.FocusedObject = controllable;
+			}
+
+			if (Program.StartupStopwatch != null)
+			{
+				Program.StartupStopwatch.Stop();
+				Trace.WriteLine(String.Format("Startup {0} ms", Program.StartupStopwatch.ElapsedMilliseconds));
+				Program.StartupStopwatch = null;
+			}
+		}
+
+		void user_DisconnectEvent()
+		{
+			this.User.DisconnectEvent -= user_DisconnectEvent;
+
+			App.GameWindow.FocusedObject = null;
+			App.GameWindow.MapControl.Environment = null;
+			this.User = null;
+			this.World = null;
+		}
+
+
 
 		#region INotifyPropertyChanged Members
 		public event PropertyChangedEventHandler PropertyChanged;
