7652874fea5efeb020e28e3fc7e22c22d91027e4
Tomi Valkeinen <tomba@iki.fi>
2013-08-29 14:09:40 +0300

    add WorldTaskHelpers

---
+++ Server/World/World/WorldTaskHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwarrowdelf.Server
+{
+	public sealed partial class World
+	{
+		public Task WaitTickEnded()
+		{
+			VerifyAccess();
+
+			if (this.IsTickOnGoing == false)
+				return Task.FromResult<bool>(true);
+
+			TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+			var action = new Action(() => tcs.SetResult(true));
+
+			this.TickEnded += action;
+
+			tcs.Task.ContinueWith(t => this.TickEnded -= action);
+
+			return tcs.Task;
+		}
+	}
+}
