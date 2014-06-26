cd8f1f94881e55370100f33d69780f9c4ac0b5c3
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2014-06-26 12:32:40 +0300

    add tileset texture

--- TexGen/Program.cs
+++ TexGen/Program.cs
@@ -21,6 +21,8 @@ namespace TexGen
 				var device = new Device(adapter, DeviceCreationFlags.None, FeatureLevel.Level_10_0);
 
 				CreateTextures(device);
+
+				CreateTileSetTextures(device);
 			}
 		}
 
@@ -79,6 +81,69 @@ namespace TexGen
 			}
 
 			Texture2D.ToFile(device.ImmediateContext, textureArray, ImageFileFormat.Dds, "TileTextureArray.dds");
+		}
+
+		static void CreateTileSetTextures(Device device)
+		{
+			var tileSet = new TileSet("../../Data/TileSet.png");
+
+			int numDistinctBitmaps = tileSet.NumTiles;
+
+			const int bytesPerPixel = 4;
+			int maxTileSize = 64;
+			int mipLevels = 6; // 64, 32, 16, 8, 4, 2
+
+			var atlasTexture = new Texture2D(device, new Texture2DDescription()
+			{
+				Usage = ResourceUsage.Default,
+				BindFlags = BindFlags.ShaderResource,
+				CpuAccessFlags = CpuAccessFlags.None,
+
+				Format = SharpDX.DXGI.Format.B8G8R8A8_UNorm,
+				Width = maxTileSize,
+				Height = maxTileSize,
+				SampleDescription = new SharpDX.DXGI.SampleDescription(1, 0),
+
+				ArraySize = numDistinctBitmaps,
+				MipLevels = mipLevels,
+			});
+
+			var bmpRaw = tileSet.GetRawBitmap();
+			int bmpWidth = tileSet.RawBitmapWidth;
+
+			int autoGenMipLevel = 0;
+
+			using (var dataStream = DataStream.Create(bmpRaw, true, false))
+			{
+				for (int mipLevel = 0; mipLevel < mipLevels; ++mipLevel)
+				{
+					int tileSize = maxTileSize >> mipLevel;
+
+					if (tileSet.HasTileSize(tileSize) == false)
+					{
+						autoGenMipLevel = mipLevel - 1;
+						break;
+					}
+
+					for (int i = 0; i < numDistinctBitmaps; ++i)
+					{
+						int xOffset = tileSet.GetTileXOffset(tileSize);
+						int yOffset = tileSet.GetTileYOffset(i);
+
+						dataStream.Position = yOffset * bmpWidth * bytesPerPixel + xOffset * bytesPerPixel;
+
+						var box = new DataBox(dataStream.PositionPointer, bmpWidth * bytesPerPixel, 0);
+
+						device.ImmediateContext.UpdateSubresource(box, atlasTexture,
+							Texture2D.CalculateSubResourceIndex(mipLevel, i, mipLevels));
+					}
+				}
+			}
+
+			// Generate mipmaps for the smallest tiles
+			atlasTexture.FilterTexture(device.ImmediateContext, autoGenMipLevel, FilterFlags.Triangle);
+
+			Texture2D.ToFile(device.ImmediateContext, atlasTexture, ImageFileFormat.Dds, "TileSetTextureArray.dds");
 		}
 	}
 }
