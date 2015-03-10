ededd240baf89dc00100854207b118891099f0b4
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2015-03-10 11:32:45 +0200

    update NetSerializer to v3.0

--- Common/Game/Serializer.cs
+++ Common/Game/Serializer.cs
@@ -7,6 +7,8 @@ namespace Dwarrowdelf
 {
 	static class Serializer
 	{
+		static NetSerializer.Serializer s_serializer;
+
 		static Serializer()
 		{
 			var dataTypes = new Type[] { typeof(IntGrid2), typeof(IntGrid2Z) };
@@ -21,17 +23,17 @@ namespace Dwarrowdelf
 			var types = dataTypes.Concat(messageTypes).Concat(objectDataTypes).Concat(changeTypes).Concat(actionTypes).Concat(events)
 				.Concat(extra).Concat(reports).Concat(tileDataEnums);
 
-			NetSerializer.Serializer.Initialize(types.ToArray());
+			s_serializer = new NetSerializer.Serializer(types.ToArray());
 		}
 
 		public static void Serialize(Stream stream, Message msg)
 		{
-			NetSerializer.Serializer.Serialize(stream, msg);
+			s_serializer.Serialize(stream, msg);
 		}
 
 		public static Message Deserialize(Stream stream)
 		{
-			object ob = NetSerializer.Serializer.Deserialize(stream);
+			object ob = s_serializer.Deserialize(stream);
 			return (Message)ob;
 		}
 	}
