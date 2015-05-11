643dcaf3581b2896566a6f34277249e7bd38ff32
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2015-05-11 13:21:03 +0300

    class for coordinate converter

---
+++ Client/Client/UI/Converters/CoordinateValueConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Dwarrowdelf.Client.UI
+{
+	class CoordinateValueConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value is Point)
+			{
+				var p = (Point)value;
+				return String.Format("{0:F2}, {1:F2}", p.X, p.Y);
+			}
+
+			if (value is DoubleVector3)
+			{
+				var p = (DoubleVector3)value;
+				return String.Format("{0:F2}, {1:F2}, {2:F2}", p.X, p.Y, p.Z);
+			}
+
+			if (value is IntVector2)
+			{
+				var p = (IntVector2)value;
+				return String.Format("{0}, {1}", p.X, p.Y);
+			}
+
+			if (value is IntVector3)
+			{
+				var p = (IntVector3)value;
+				return String.Format("{0}, {1}, {2}", p.X, p.Y, p.Z);
+			}
+
+			return "Unknown";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- Client/Client/UI/MainWindow/MainWindowMouseStatusBar.xaml.cs
+++ Client/Client/UI/MainWindow/MainWindowMouseStatusBar.xaml.cs
@@ -22,41 +22,4 @@ namespace Dwarrowdelf.Client.UI
 			InitializeComponent();
 		}
 	}
-
-	class CoordinateValueConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			if (value is Point)
-			{
-				var p = (Point)value;
-				return String.Format("{0:F2}, {1:F2}", p.X, p.Y);
-			}
-
-			if (value is DoubleVector3)
-			{
-				var p = (DoubleVector3)value;
-				return String.Format("{0:F2}, {1:F2}, {2:F2}", p.X, p.Y, p.Z);
-			}
-
-			if (value is IntVector2)
-			{
-				var p = (IntVector2)value;
-				return String.Format("{0}, {1}", p.X, p.Y);
-			}
-
-			if (value is IntVector3)
-			{
-				var p = (IntVector3)value;
-				return String.Format("{0}, {1}, {2}", p.X, p.Y, p.Z);
-			}
-
-			return "Unknown";
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			throw new NotImplementedException();
-		}
-	}
 }
