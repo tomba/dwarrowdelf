4eb1bc27c1d036b3b70c5cd68aa93bf962365bd4
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2015-05-11 18:21:37 +0300

    move MapSelection

--- Client/Client/UI/MapControl/MapControlSelectionService.cs
+++ Client/Client/UI/MapControl/MapControlSelectionService.cs
@@ -11,14 +11,6 @@ using System.Windows.Shapes;
 
 namespace Dwarrowdelf.Client.UI
 {
-	enum MapSelectionMode
-	{
-		None,
-		Point,
-		Rectangle,
-		Box,
-	}
-
 	sealed class MapControlSelectionService
 	{
 		MasterMapControl m_mapControl;
@@ -426,69 +418,6 @@ namespace Dwarrowdelf.Client.UI
 			m_cursorRectangle.StrokeThickness = thickness;
 
 			m_cursorRectangle.Visibility = Visibility.Visible;
-		}
-	}
-
-	struct MapSelection
-	{
-		public MapSelection(IntVector3 start, IntVector3 end)
-			: this()
-		{
-			this.SelectionStart = start;
-			this.SelectionEnd = end;
-			this.IsSelectionValid = true;
-		}
-
-		public MapSelection(IntGrid3 box)
-			: this()
-		{
-			if (box.Columns == 0 || box.Rows == 0 || box.Depth == 0)
-			{
-				this.IsSelectionValid = false;
-			}
-			else
-			{
-				this.SelectionStart = box.Corner1;
-				this.SelectionEnd = box.Corner2;
-				this.IsSelectionValid = true;
-			}
-		}
-
-		public bool IsSelectionValid { get; set; }
-		public IntVector3 SelectionStart { get; set; }
-		public IntVector3 SelectionEnd { get; set; }
-
-		public IntVector3 SelectionPoint
-		{
-			get
-			{
-				return this.SelectionStart;
-			}
-		}
-
-		public IntGrid3 SelectionBox
-		{
-			get
-			{
-				if (!this.IsSelectionValid)
-					return new IntGrid3();
-
-				return new IntGrid3(this.SelectionStart, this.SelectionEnd);
-			}
-		}
-
-		public IntGrid2Z SelectionIntRectZ
-		{
-			get
-			{
-				if (!this.IsSelectionValid)
-					return new IntGrid2Z();
-
-				if (this.SelectionStart.Z != this.SelectionEnd.Z)
-					throw new Exception();
-
-				return new IntGrid2Z(this.SelectionStart.ToIntVector2(), this.SelectionEnd.ToIntVector2(), this.SelectionStart.Z);
-			}
 		}
 	}
 }
---
+++ Client/Common/MapSelection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwarrowdelf.Client
+{
+	public enum MapSelectionMode
+	{
+		None,
+		Point,
+		Rectangle,
+		Box,
+	}
+
+	public struct MapSelection
+	{
+		public MapSelection(IntVector3 start, IntVector3 end)
+			: this()
+		{
+			this.SelectionStart = start;
+			this.SelectionEnd = end;
+			this.IsSelectionValid = true;
+		}
+
+		public MapSelection(IntGrid3 box)
+			: this()
+		{
+			if (box.Columns == 0 || box.Rows == 0 || box.Depth == 0)
+			{
+				this.IsSelectionValid = false;
+			}
+			else
+			{
+				this.SelectionStart = box.Corner1;
+				this.SelectionEnd = box.Corner2;
+				this.IsSelectionValid = true;
+			}
+		}
+
+		public bool IsSelectionValid { get; set; }
+		public IntVector3 SelectionStart { get; set; }
+		public IntVector3 SelectionEnd { get; set; }
+
+		public IntVector3 SelectionPoint
+		{
+			get
+			{
+				return this.SelectionStart;
+			}
+		}
+
+		public IntGrid3 SelectionBox
+		{
+			get
+			{
+				if (!this.IsSelectionValid)
+					return new IntGrid3();
+
+				return new IntGrid3(this.SelectionStart, this.SelectionEnd);
+			}
+		}
+
+		public IntGrid2Z SelectionIntRectZ
+		{
+			get
+			{
+				if (!this.IsSelectionValid)
+					return new IntGrid2Z();
+
+				if (this.SelectionStart.Z != this.SelectionEnd.Z)
+					throw new Exception();
+
+				return new IntGrid2Z(this.SelectionStart.ToIntVector2(), this.SelectionEnd.ToIntVector2(), this.SelectionStart.Z);
+			}
+		}
+	}
+}
